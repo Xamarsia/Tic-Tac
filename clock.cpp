855e96c465129a145a02f6677dedc91d26029bce
Xamarsia <xamarsia@gmail.com>
2021-03-28 23:47:34 +0300

    initialization: create program with two synchronized threads

---
+++ clock.cpp
@@ -0,0 +1,62 @@
+#include <mutex>
+#include <condition_variable>
+#include <iostream>
+#include <thread>
+
+class Clock
+{
+private:
+    enum Status
+    {
+        TIC,
+        TAC
+    };
+
+    Status clockStatus = Status::TAC;
+    std::mutex status;
+    std::condition_variable cv;
+    int runCount;
+
+public:
+    Clock(int count) : runCount(count) {}
+
+    void tic()
+    {
+        for (int i = 0; i < runCount; i++)
+        {
+            std::unique_lock<std::mutex> ul(status);
+            cv.wait(ul, [=]() { return clockStatus == Status::TAC; });
+            clockStatus = Status::TIC;
+            std::cout << "TIC" << std::endl;
+            cv.notify_one();
+        }
+    }
+
+    void tac()
+    {
+        for (int i = 0; i < runCount; i++)
+        {
+            std::unique_lock<std::mutex> ul(status);
+            cv.wait(ul, [=]() { return clockStatus == Status::TIC; });
+            clockStatus = Status::TAC;
+            std::cout << "\tTAC" << std::endl;
+            cv.notify_one();
+        }
+    }
+
+
+    void run()
+    {
+        std::thread ticThread(&Clock::tic, this);
+        std::thread tacThread(&Clock::tac, this);
+        ticThread.join();
+        tacThread.join();
+    }
+};
+
+int main()
+{
+    Clock clock(10);
+    clock.run();
+    return 0;
+}
